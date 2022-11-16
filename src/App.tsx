6130927abf5916e41655cb9fb9f9d47732865421
UgurcanSARICI <ugurcansarici@gmail.com>
2022-11-16 21:19:50 +0300

    skills

--- src/App.tsx
+++ src/App.tsx
@@ -12,11 +12,11 @@ const App = () => {
     <>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/skills' element={<Skills />} />
-        <Route path='/experience' element={<Experience />} />
-        <Route path='/projects' element={<Projects />} />
-        <Route path='/contact' element={<Contact />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/skills" element={<Skills />} />
+        {/* <Route path='/experience' element={<Experience />} /> */}
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </>
   );
