fa74b288f807b4afbf0a854c31a325cf2753daef
UgurcanSARICI <ugurcansarici@gmail.com>
2022-11-16 20:52:16 +0300

    first commit

---
+++ src/data/AboutData.ts
@@ -0,0 +1,13 @@
+interface AboutType {
+  firstName: string;
+  lastName: string;
+  about1: string;
+  about2: string;
+}
+
+export const AboutData: AboutType = {
+  firstName: "Ugurcan",
+  lastName: "SARICI",
+  about1: "<A Developer who loves to build cool things />",
+  about2: "<Frontend Developer />",
+};
---
+++ src/data/ProjectsData.ts
@@ -0,0 +1,24 @@
+interface ProjectsType {
+  title: string;
+  description: string;
+  image: string;
+  GitHub: string;
+  hosted: string;
+}
+
+export const ProjectsData: ProjectsType[] = [
+  {
+    title: "Portfolio | Private Project",
+    description: `Fastest Landing page built with React, Next Js and Styled Components. modern monorepo architecture. Minimal designa`,
+    image: "https://i.imgur.com/iovm1R9.png",
+    GitHub: "https://github.com/erfjs/animated-portfolio",
+    hosted: "https://erfjs.com",
+  },
+  {
+    title: "Portfolio | Private Project",
+    description: `Fastest Landing page built with React, Next Js and Styled Components. modern monorepo architecture. Minimal design`,
+    image: "https://i.imgur.com/iovm1R9.png",
+    GitHub: "https://github.com/erfjs/animated-portfolio",
+    hosted: "https://erfjs.com",
+  },
+];
